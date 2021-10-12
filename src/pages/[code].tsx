12c468d8fe7efcc0c9aeb074aba168f00e594788
Tika <captika@outlook.com>
2021-10-12 20:20:30 +0000

    feat: game ended screen

--- src/pages/[code].tsx
+++ src/pages/[code].tsx
@@ -121,6 +121,45 @@ export default function Game(props: GameProps) {
     return game.hand0 !== null && game.hand1 !== null;
   }
 
+  function getEndText() {
+    if (!game) return <h1></h1>;
+
+    const winner =
+      game.cards0.length > game.cards1.length
+        ? game.players[0].name
+        : game.players[1].name;
+    const loser =
+      game.players[0].name === winner
+        ? game.players[1].name
+        : game.players[0].name;
+    const pointDifference = Math.abs(game.cards0.length - game.cards1.length);
+
+    console.log(me, game.cards0.length, game.cards1.length);
+
+    if (me === 0 && game.cards0.length > game.cards1.length) {
+      return (
+        <h1>
+          you <span style={{ color: "var(--green)" }}>won</span> by{" "}
+          {pointDifference} points against <span>{loser}</span>
+        </h1>
+      );
+    } else if (me === 1 && game.cards1.length > game.cards0.length) {
+      return (
+        <h1>
+          you <span style={{ color: "var(--green)" }}>won</span> by{" "}
+          {pointDifference} points against <span>{loser}</span>
+        </h1>
+      );
+    } else {
+      return (
+        <h1>
+          you <span style={{ color: "var(--red)" }}>lost</span> by{" "}
+          {pointDifference} points to <span>{winner}</span>
+        </h1>
+      );
+    }
+  }
+
   return (
     <div className={styles.bg}>
       {!game ? (
@@ -153,14 +192,31 @@ export default function Game(props: GameProps) {
         <>
           {game.deck.length === 0 ? (
             <div className={styles.ended}>
-              <h1>
-                you
-                {game.cards0.length > game.cards1.length
-                  ? game.players[0].name
-                  : game.players[1].name}{" "}
-                won!
-              </h1>
-              <h2>you lost to {game.players[0].name}</h2>
+              {getEndText()}
+              <div className={styles.score}>
+                <h1 style={{ fontSize: "1.5em" }}>
+                  {me === 0 ? game.cards0.length : game.cards1.length}
+                </h1>
+                <h2 style={{ fontSize: "1.5em" }}>:</h2>
+                <h1 style={{ fontSize: "1.5em" }}>
+                  {me === 1 ? game.cards0.length : game.cards1.length}
+                </h1>
+              </div>
+
+              <button
+                className={generalStyles.iconButton}
+                style={{
+                  width: "10em",
+                  position: "absolute",
+                  bottom: "4em",
+                }}
+                onClick={() => {
+                  router.push("/");
+                }}
+              >
+                <LogOut size={25} color="white" />
+                go home
+              </button>
             </div>
           ) : (
             <div className={styles.game}>
